11b7926df1b86584d55b36dd554dc522c3ce358f
lioneluchiha <138074981+lioneluchiha@users.noreply.github.com>
2024-03-01 20:48:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,9 +4,9 @@ const fs = require("fs")
 //aumto presence update
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.autoblockmorroco = false //auto block 212 (true to on, false to off)
-global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
-global.antispam = false //auto kick spammer (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = true //auto kick spammer (true to on, false to off)
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
